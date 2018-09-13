71fa9699c659c20b06f42f8426dbeb5d056d6606
Johanna <Kenney@Johannas-MacBook-Pro.local>
2018-09-13 15:45:48 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Johannas Test',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Out of the depths and furthest reaches</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
